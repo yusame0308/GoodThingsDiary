ed282a8b1045fa7609b5cc0442bf9726e304c1a5
yusame <yu.starts.yu@gmail.com>
2023-03-07 02:02:10 +0900

    tableViewのtopのsafeArea対応の方法をsafeAreaLayoutGuideに変更

--- Zenko/ViewControllers/HomeViewController.swift
+++ Zenko/ViewControllers/HomeViewController.swift
@@ -32,12 +32,7 @@ class HomeViewController: UIViewController {
         
         view.addSubview(postTableView)
         
-        postTableView.anchor(bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        postTableView.anchor(top: view.topAnchor, topPadding: view.safeAreaInsets.top)
+        postTableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
     }
 
 }
