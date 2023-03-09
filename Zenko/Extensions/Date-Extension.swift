d197a6b516ac56e14d55a6bbd89da9c9676c4fc4
yusame <yu.starts.yu@gmail.com>
2023-03-09 20:36:05 +0900

    Date-Extensionを作成

---
+++ Zenko/Extensions/Date-Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Date-Extension.swift
+//  Zenko
+//
+//  Created by 小原宙 on 2023/03/09.
+//
+
+import Foundation
+
+extension Date {
+    
+    func toString() -> String {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        formatter.locale = Locale(identifier: "ja_JP")
+        formatter.dateFormat = "M月d日 H時m分"
+        
+        return formatter.string(from: self)
+    }
+    
+}
